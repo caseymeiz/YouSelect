4b822289a30fbda72fe3b7a9b00d761428e63c5f
Casey Meisenzahl <caseymeiz@gmail.com>
2015-08-07 17:24:25 -0400

    use self instead of object name

--- youSelect.js
+++ youSelect.js
@@ -7,34 +7,35 @@ YouSelect = {
 	},
 
 	init: function (){
-		s = this.settings;
-		this.bindEvents();
+		self = this;
+		s = self.settings;
+		self.bindEvents();
 	},
 
 	bindEvents: function (){
-		chrome.runtime.onMessage.addListener(this.deactivate);
-		window.addEventListener("keyup",this.handle_key);
+		chrome.runtime.onMessage.addListener(self.deactivate);
+		window.addEventListener("keyup",self.handle_key);
 	},
 
 	handle_key: function (event){
 		if (s.is_active){
 			switch (event.keyCode) {
 				case (40): //down arrow
-					YouSelect.select_link(1);
+					self.select_link(1);
 					break;
 				case (38): //up arrow
-					YouSelect.select_link(-1);
+					self.select_link(-1);
 					break;
 			}
 		} else if (event.keyCode === 9) { // tab
-			YouSelect.activate();
+			self.activate();
 		}
 	},
 
 	activate: function() {
 		s.is_active = true;
-		this.cache_links();
-		this.select_link();
+		self.cache_links();
+		self.select_link();
 	},
 
 	deactivate: function() {
@@ -62,10 +63,10 @@ YouSelect = {
 	select_link: function(change){
 		var temporary_index = (s.index + change) || 0;
 		if (s.index%5 === 0){
-			this.cache_links();
+			self.cache_links();
 		}
 		if(temporary_index >= 0 && temporary_index <= s.links.length - 1){
-			this.deselect_link();
+			self.deselect_link();
 			s.index = temporary_index;
 			if (s.links[s.index]){
 				s.links[s.index].id = 'youSelect';
